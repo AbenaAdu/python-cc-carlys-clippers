c2c510d1e4e9bf4436b6f2d63532c580b2fa27fd
Abena Adu <abeanadu@gmail.com>
2024-04-09 16:29:22 -0400

    Added all prices

--- clippers.py
+++ clippers.py
@@ -12,3 +12,9 @@ hairstyles = [
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+#Add all the prices 
+total_price = 0
+for price in prices:
+  total_price += price
+  
