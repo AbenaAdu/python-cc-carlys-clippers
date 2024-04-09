920208f2fa5ee9d8a0f2a02f3ae1f9665f0c4221
Abena Adu <abeanadu@gmail.com>
2024-04-09 16:36:47 -0400

    Average prices

--- clippers.py
+++ clippers.py
@@ -17,4 +17,7 @@ last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 total_price = 0
 for price in prices:
   total_price += price
-  
+
+#Average price of prices
+average_price = total_price / len(prices)
+print("Average Haircut Price: " + str(average_price))
